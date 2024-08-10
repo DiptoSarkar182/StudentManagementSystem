f4ec53588db87b480f8ec8ce709c911d9d0bba5f
DiptoSarkar182 <diptomanchester2018@gmail.com>
2024-08-10 17:42:33 +0600

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManagementSystem.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = null!;
+
+        public string MIddletName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+
+        public string FullName => $"{FirstName} {MIddletName} {LastName}";
+
+        public string EmailAddress { get; set; } = null!;
+
+        public int PhoneNumber { get; set; }
+        public string Gender { get; set; } = null!;
+    }
+}
